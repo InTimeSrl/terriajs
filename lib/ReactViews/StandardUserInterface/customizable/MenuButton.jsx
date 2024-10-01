02b3740245b2304d0da4e5dd8e338b9ef60b2413
arturo.lebani2 <arturo.lebani2@gmial.com>
2024-10-01 12:02:45 +0200

    mod menu button

--- lib/ReactViews/StandardUserInterface/customizable/MenuButton.jsx
+++ lib/ReactViews/StandardUserInterface/customizable/MenuButton.jsx
@@ -1,3 +1,43 @@
+// import React from "react";
+// import classNames from "classnames";
+// import Icon from "../../../Styled/Icon";
+// import PropTypes from "prop-types";
+// import Styles from "./menu-button.scss";
+
+// /**
+//  * Basic button for use in the menu part at the top of the map.
+//  *
+//  * @constructor
+//  */
+// function MenuButton(props) {
+//   const target = props.href !== "#" ? "_blank" : undefined;
+//   const rel = target === "_blank" ? "noreferrer" : undefined;
+//   return (
+//     <a
+//       className={classNames(Styles.btnAboutLink, {
+//         [Styles.aboutTweak]: props.href === "about.html"
+//       })}
+//       href={props.href}
+//       target={target}
+//       rel={rel}
+//       title={props.caption}
+//     >
+//       {props.href !== "#" && <Icon glyph={Icon.GLYPHS.externalLink} />}
+//       <span>{props.caption}</span>
+//     </a>
+//   );
+// }
+
+// MenuButton.defaultProps = {
+//   href: "#"
+// };
+
+// MenuButton.propTypes = {
+//   href: PropTypes.string,
+//   caption: PropTypes.string.isRequired
+// };
+
+// export default MenuButton;
 import React from "react";
 import classNames from "classnames";
 import Icon from "../../../Styled/Icon";
@@ -10,31 +50,61 @@ import Styles from "./menu-button.scss";
  * @constructor
  */
 function MenuButton(props) {
-  const target = props.href !== "#" ? "_blank" : undefined;
+  const { href, caption, icon, onClick } = props;
+
+  const target = href !== "#" ? "_blank" : undefined;
   const rel = target === "_blank" ? "noreferrer" : undefined;
+
+  // Determine if the icon is a built-in glyph or a custom component
+  const renderIcon = () => {
+    if (typeof icon === "string") {
+      return <Icon glyph={icon} />;
+    } else if (React.isValidElement(icon)) {
+      return icon;
+    } else {
+      return <Icon glyph={Icon.GLYPHS.externalLink} />;
+    }
+  };
+
+  // Handle click event
+  const handleClick = (event) => {
+    if (onClick) {
+      event.preventDefault(); // Prevent default anchor behavior if onClick is provided
+      onClick(event);
+    }
+  };
+
   return (
     <a
       className={classNames(Styles.btnAboutLink, {
-        [Styles.aboutTweak]: props.href === "about.html"
+        [Styles.aboutTweak]: href === "about.html",
       })}
-      href={props.href}
+      href={href}
       target={target}
       rel={rel}
-      title={props.caption}
+      title={caption}
+      onClick={handleClick} // Attach the handleClick function to the onClick event
     >
-      {props.href !== "#" && <Icon glyph={Icon.GLYPHS.externalLink} />}
-      <span>{props.caption}</span>
+      {renderIcon()}
+      <span>{caption}</span>
     </a>
   );
 }
 
 MenuButton.defaultProps = {
-  href: "#"
+  href: "#",
+  icon: Icon.GLYPHS.externalLink, // Default icon
+  onClick: null, // Default to no-op
 };
 
 MenuButton.propTypes = {
   href: PropTypes.string,
-  caption: PropTypes.string.isRequired
+  caption: PropTypes.string.isRequired,
+  icon: PropTypes.oneOfType([
+    PropTypes.string, // For built-in icons
+    PropTypes.node, // For custom React components
+  ]),
+  onClick: PropTypes.func, // Function to handle click event
 };
 
 export default MenuButton;
